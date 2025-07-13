ed0d5bb400d2d230e3a9e970a3d1fb9070978ffe
Bence Kozma <bencejob@gmail.com>
2025-07-13 16:55:02 +0100

    update dev script

--- dev.sh
+++ dev.sh
@@ -4,4 +4,4 @@ if [ -z "$(docker images -q terraform-builder:latest 2> /dev/null)" ]; then
   docker build -t 'terraform-builder' .
 fi
 
-docker run --rm -it -v .:/workspace terraform-builder
+docker run --rm -it -v .:/app terraform-builder
